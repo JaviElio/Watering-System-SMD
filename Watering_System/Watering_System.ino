6abdbd8001d27a50ca1ab75e1100075c36f3f059
JaviElio <javielio@hotmail.com>
2017-08-18 18:32:45 +0200

    Some tests

--- Watering_System/Watering_System.ino
+++ Watering_System/Watering_System.ino
@@ -24,84 +24,54 @@
 #define ESP_OFF     PORTA &= ~(1 << PORTA7)
 #define RX          PORTA3
 #define TX          PORTA2
-#define BAUDRATE    115200
+#define BAUDRATE    9600
 
 
 // OBJECTS
 I2CSoilMoistureSensor sensor(0x20);
-SoftwareSerial mySerial(RX,TX);
-SoftwareSerial debug(-1,PORTA4);
+SoftwareSerial mySerial(RX, TX);
+SoftwareSerial debug(-1, PORTA4);
 
 
-//VARIABLES
-char bufferString[65]={'*'};
-uint8_t index=0;
-const char msgInit[] = "OK"; 
+//MESSAGES
+const char msgInit[] = "OK";
 
 void setup() {
-  // put your setup code here, to run once:
+ 
+  DDRB |= (1 << DDB0);      // B0 -> Output
+  DDRA |= (1 << DDA7);      // A7 -> Output
 
 
   mySerial.begin(BAUDRATE);     // Init serial comm.
-  debug.begin(BAUDRATE);
-  //TinyWireM.begin();        // Init i2c comm.
+  debug.begin(BAUDRATE);        // Init debug wire
+  //TinyWireM.begin();          // Init i2c comm.
 
 
-  DDRB |= (1 << DDB0);      // B0 -> Output
-  DDRA |= (1 << DDA7);      // A7 -> Output
-  //SENSOR_ON;
+  //ESP8266 ON;
   ESP_ON;
-  delay(1000);
-
-  
-  clearBuffer();
-  mySerial.println("AT");
-  debug.println("debug: AT");
-  delay(1000);
+  delay(500);
 
 
-  if ( waitForString(msgInit,2,2000)) {
-    debug.println("Init ok");
-  }
-  else {
-    debug.println("Init nok");
-  }
-  
-  
-  debug.println(mySerial.available());
-
-/*
-  while (mySerial.available()) {
-      int currentByte = mySerial.read();
-      bufferString[index] = currentByte;
-      debug.print(bufferString[index]);
-   }
-*/
-   
-  debug.println();
-  debug.println( mySerial.available());
-
-     
 }
 
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
- 
+  clearBuffer();
+  mySerial.println("AT");
+  bool initOK = waitForString("OK",2,2000);
+
+  debug.println();
+  if (initOK) debug.println("initOK");
+  else debug.println("initNoOK");
+
+
+  delay(5000);
+
+
+
 }
-
-
-
-//Remove all bytes from the buffer
-void clearBuffer() {
-
-  while (mySerial.available())
-    mySerial.read();
-}
-
-
 
 
 
@@ -112,30 +82,38 @@ bool waitForString(char* input, uint8_t length, unsigned int timeout) {
   uint8_t index = 0;
 
   while (end_time >= millis()) {
-    
-      if(mySerial.available()) {
-        
-        //Read one byte from serial port
-        current_byte = mySerial.read();
 
-        if (current_byte != -1) {
-          //Search one character at a time
-          if (current_byte == input[index]) {
-            index++;
-            
-            //Found the string
-            if (index == length) {              
-              return true;
-            }
-          //Restart position of character to look for
-          } else {
-            index = 0;
+    if (mySerial.available()) {
+
+      //Read one byte from serial port
+      current_byte = mySerial.read();
+      debug.print(char(current_byte));
+
+      if (current_byte != -1) {
+        //Search one character at a time
+        if (current_byte == input[index]) {
+          index++;
+
+          //Found the string
+          if (index == length) {
+            return true;
           }
+          //Restart position of character to look for
+        } else {
+          index = 0;
         }
       }
-  }  
+    }
+  }
   //Timed out
+
   return false;
 }
 
+//Remove all bytes from the buffer
+void clearBuffer() {
+
+  while (mySerial.available())
+    mySerial.read();
+}
 
